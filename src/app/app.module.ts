b54a040595a727364c4fefc0c34f25d52ce3130a
Benoit Chevalier <benoit.chevalier@ge.com>
2018-05-23 09:28:12 -0700

    compile, first pass at docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
+import { ZzButtonComponent } from './zz-button/zz-button.component';
+import { ZzButtonnComponent } from './zz-buttonn/zz-buttonn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZzButtonComponent,
+    ZzButtonnComponent
+  ],
+  entryComponents: [
+    ZzButtonComponent,
+    ZzButtonnComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +22,12 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+  ngDoBootstrap() {
+    const el = createCustomElement(ZzButtonComponent, { injector: this.injector }),
+    el2 = createCustomElement(ZzButtonnComponent, { injector: this.injector });
+    customElements.define('zz-button', el);
+    customElements.define('zz-buttonn', el2);
+  }
+}
